abb42d5c54f6d7a7ed1f40c383fdf68430040b7f
bennethade <92984648+bennethade@users.noreply.github.com>
2025-07-22 01:17:25 +0100

    product filter added

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,17 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::orderBy('created_at', 'desc')->paginate(12);
+        $size = $request->query('size') ? $request->query('size') : 12;
 
-        return view('shop', compact('products'));
+        $orderColumn = "";
+
+        $o_order = "";
+
+        $order = $request->query('order') ? $request->query('order') : -1;
+
+        $filterBrands = $request->query('brands');
+
+        $filterCategories = $request->query('categories');
+
+        $minPrice = $request->query('min') ? $request->query('min') : 1;
+        $maxPrice = $request->query('max') ? $request->query('max') : 5000;
+
+        switch($order)
+        {
+            case 1:
+                $orderColumn = 'created_at';
+                $o_order = 'DESC';
+                break;
+            case 2:
+                $orderColumn = 'created_at';
+                $o_order = 'ASC';
+                break;
+            case 3:
+                $orderColumn = 'sale_price';
+                $o_order = 'ASC';
+                break;
+            case 4:
+                $orderColumn = 'sale_price';
+                $o_order = 'DESC';
+                break;
+            default:
+                $orderColumn = 'id';
+                $o_order = 'DESC';
+        }
+
+        $brands = Brand::orderBy('name', 'ASC')->get();
+
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+        $products = Product::where(function($query) use($filterBrands){
+                $query->whereIn('brand_id', explode(',',$filterBrands))->orWhereRaw("'".$filterBrands."'=''");
+            })
+            ->where(function($query) use($filterCategories){
+                $query->whereIn('category_id', explode(',',$filterCategories))->orWhereRaw("'".$filterCategories."'=''");
+            })
+
+            ->where(function($query) use($minPrice, $maxPrice){
+                $query->whereBetween('regular_price', [$minPrice, $maxPrice])
+                        ->orWhereBetween('sale_price', [$minPrice, $maxPrice]);
+            })
+            ->orderBy($orderColumn, $o_order)->paginate($size);
+
+        return view('shop', compact('products', 'size', 'order', 'brands', 'filterBrands', 'categories', 'filterCategories', 'minPrice', 'maxPrice'));
     }
+
+
 
     public function productDetails($product_slug)
     {
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
+    <style>
+        .brand-list li, .category-list li{
+            line-height: 40px;
+        }
+
+        /* .brand-list li, .chk-brand, .category-list li .chk-category{ */
+        .chk-brand, .category-list li .chk-category{
+            width: 1rem;
+            height: 1rem;
+            color: #e4e4e4;
+            border: 0.125rem solid currentColor;
+            border-radius: 0;
+            margin-right: 0.75rem;
+        }
+    </style>
+
     <main class="pt-90">
         <section class="shop-main container d-flex pt-4 pt-xl-5">
         <div class="shop-sidebar side-sticky bg-body" id="shopFilter">
@@ -28,38 +44,18 @@
                 </h5>
                 <div id="accordion-filter-1" class="accordion-collapse collapse show border-0"
                 aria-labelledby="accordion-heading-1" data-bs-parent="#categories-list">
-                <div class="accordion-body px-0 pb-0 pt-3">
+                <div class="accordion-body px-0 pb-0 pt-3 category-list">
                     <ul class="list list-inline mb-0">
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Dresses</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Shorts</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Sweatshirts</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Swimwear</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Jackets</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">T-Shirts & Tops</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Jeans</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Trousers</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Men</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="#" class="menu-link py-1">Jumpers & Cardigans</a>
-                    </li>
+                        @foreach ($categories as $category)
+                            <li class="list-item">
+                                <span class="menu-link py-1">
+                                    <input type="checkbox" class="chk-category" name="categories" id="" value="{{ $category->id }}"
+                                        @if (in_array($category->id, explode(',',$filterCategories))) checked="checked" @endif>
+                                    {{ $category->name }}
+                                </span>
+                                <span class="text-right float-end">{{ $category->products->count() }}</span>
+                            </li>        
+                        @endforeach
                     </ul>
                 </div>
                 </div>
@@ -134,69 +130,39 @@
 
 
             <div class="accordion" id="brand-filters">
-            <div class="accordion-item mb-4 pb-3">
-                <h5 class="accordion-header" id="accordion-heading-brand">
-                <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#accordion-filter-brand" aria-expanded="true" aria-controls="accordion-filter-brand">
-                    Brands
-                    <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
-                    <g aria-hidden="true" stroke="none" fill-rule="evenodd">
-                        <path
-                        d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
-                    </g>
-                    </svg>
-                </button>
-                </h5>
-                <div id="accordion-filter-brand" class="accordion-collapse collapse show border-0"
-                aria-labelledby="accordion-heading-brand" data-bs-parent="#brand-filters">
-                <div class="search-field multi-select accordion-body px-0 pb-0">
-                    <select class="d-none" multiple name="total-numbers-list">
-                    <option value="1">Adidas</option>
-                    <option value="2">Balmain</option>
-                    <option value="3">Balenciaga</option>
-                    <option value="4">Burberry</option>
-                    <option value="5">Kenzo</option>
-                    <option value="5">Givenchy</option>
-                    <option value="5">Zara</option>
-                    </select>
-                    <div class="search-field__input-wrapper mb-3">
-                    <input type="text" name="search_text"
-                        class="search-field__input form-control form-control-sm border-light border-2"
-                        placeholder="Search" />
+                <div class="accordion-item mb-4 pb-3">
+                    <h5 class="accordion-header" id="accordion-heading-brand">
+                    <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#accordion-filter-brand" aria-expanded="true" aria-controls="accordion-filter-brand">
+                        Brands
+                        <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                        <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                            <path
+                            d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                        </g>
+                        </svg>
+                    </button>
+                    </h5>
+                    <div id="accordion-filter-brand" class="accordion-collapse collapse show border-0"
+                        aria-labelledby="accordion-heading-brand" data-bs-parent="#brand-filters">
+                        <div class="search-field multi-select accordion-body px-0 pb-0">
+                            <ul class="list list-inline mb-0 brand-list">
+                                @foreach ($brands as $brand)
+                                    <li class="list-item">
+                                        <span class="menu-link py-1">
+                                            <input type="checkbox" name="brands" id="" value="{{ $brand->id }}" class="chk-brand" 
+                                                @if (in_array($brand->id, explode(',',$filterBrands))) checked="checked" @endif>
+                                            {{ $brand->name }}
+                                        </span>
+                                        <span class="text-right float-end">
+                                            {{ $brand->products->count() }}
+                                        </span>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
-                    <ul class="multi-select__list list-unstyled">
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Adidas</span>
-                        <span class="text-secondary">2</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Balmain</span>
-                        <span class="text-secondary">7</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Balenciaga</span>
-                        <span class="text-secondary">10</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Burberry</span>
-                        <span class="text-secondary">39</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Kenzo</span>
-                        <span class="text-secondary">95</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Givenchy</span>
-                        <span class="text-secondary">1092</span>
-                    </li>
-                    <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                        <span class="me-auto">Zara</span>
-                        <span class="text-secondary">48</span>
-                    </li>
-                    </ul>
                 </div>
-                </div>
-            </div>
             </div>
 
 
@@ -215,19 +181,19 @@
                 </button>
                 </h5>
                 <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
-                aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
-                <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="10"
-                    data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]" data-currency="$" />
-                <div class="price-range__info d-flex align-items-center mt-2">
-                    <div class="me-auto">
-                    <span class="text-secondary">Min Price: </span>
-                    <span class="price-range__min">$250</span>
+                    aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
+                    <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="1"
+                        data-slider-max="5000" data-slider-step="5" data-slider-value="[{{ $minPrice }}, {{ $maxPrice }}]" data-currency="$" />
+                    <div class="price-range__info d-flex align-items-center mt-2">
+                        <div class="me-auto">
+                        <span class="text-secondary">Min Price: </span>
+                        <span class="price-range__min">$1</span>
+                        </div>
+                        <div>
+                        <span class="text-secondary">Max Price: </span>
+                        <span class="price-range__max">$5000</span>
+                        </div>
                     </div>
-                    <div>
-                    <span class="text-secondary">Max Price: </span>
-                    <span class="price-range__max">$450</span>
-                    </div>
-                </div>
                 </div>
             </div>
             </div>
@@ -328,17 +294,19 @@
             </div>
 
             <div class="shop-acs d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
-                <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items"
-                name="total-number">
-                <option selected>Default Sorting</option>
-                <option value="1">Featured</option>
-                <option value="2">Best selling</option>
-                <option value="3">Alphabetically, A-Z</option>
-                <option value="3">Alphabetically, Z-A</option>
-                <option value="3">Price, low to high</option>
-                <option value="3">Price, high to low</option>
-                <option value="3">Date, old to new</option>
-                <option value="3">Date, new to old</option>
+                <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Page Size" id="pagesize" name="pagesize" style="margin-right: 20px;">
+                    <option value="12" {{ $size == 12 ? 'selected' : ''  }}>Show</option>
+                    <option value="24" {{ $size == 24 ? 'selected' : ''  }}>24</option>
+                    <option value="48" {{ $size == 48 ? 'selected' : ''  }}>48</option>
+                    <option value="102" {{ $size == 102 ? 'selected' : ''  }}>102</option>
+                </select>
+
+                <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0" aria-label="Sort Items" name="orderby" id="orderby">
+                    <option value="-1" {{ $order == -1 ? 'selected' : '' }}>Default</option>
+                    <option value="1" {{ $order == 1 ? 'selected' : '' }}>Date, New To Old</option>
+                    <option value="2" {{ $order == 2 ? 'selected' : '' }}>Date, Old To New</option>
+                    <option value="3" {{ $order == 3 ? 'selected' : '' }}>Price, Low To High</option>
+                    <option value="4" {{ $order == 4 ? 'selected' : '' }}>Price, High To Low</option>
                 </select>
 
                 <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -404,7 +372,7 @@
                         </div>
 
                         <div class="pc__info position-relative">
-                            <p class="pc__category">${{ $product->category->name }}</p>
+                            <p class="pc__category">{{ $product->category->name }}</p>
                             <h6 class="pc__title"><a href="{{ route('shop.product.details', $product->slug) }}">{{ $product->name }}</a></h6>
                             <div class="product-card__price d-flex">
                             <span class="money price">
@@ -451,11 +419,89 @@
             <div class="divider"></div>
 
             <div class="flex item-center justify-between flex-wrap gap-10 wgp-pagination">
-                {{ $products->links('pagination::bootstrap-5') }}
+                {{ $products->withQueryString()->links('pagination::bootstrap-5') }}
             </div>
             
         </div>
         </section>
     </main>
 
+
+    <form id="frmfilter" method="GET" action="{{ route('shop.index') }}">
+        <input type="hidden" name="page" value="{{ $products->currentPage() }}">
+        <input type="hidden" name="size" id="size" value="{{ $size }}">
+        <input type="hidden" name="order" id="order" value="{{ $order }}">
+        <input type="hidden" name="brands" id="hdnBrands">
+        <input type="hidden" name="categories" id="hdnCategories">
+        <input type="hidden" name="min" id="hdnMinPrice" value="{{ $minPrice }}">
+        <input type="hidden" name="max" id="hdnMaxPrice" value="{{ $maxPrice }}">
+    </form>
+
 @endsection
+
+
+@push('scripts')
+    <script>
+        $(function() {
+            // FOR PAGE ORDERING BY NUMBER
+            $("#pagesize").on("change", function(){
+                $("#size").val($("#pagesize option:selected").val());
+                $("#frmfilter").submit();
+            });
+
+            // FOR PAGE ORDERING BY DATE AND PRICE
+            $("#orderby").on("change", function(){
+                $("#order").val($("#orderby option:selected").val());
+                $("#frmfilter").submit();
+            });
+
+
+            // FOR BRANDS FILTER
+            $("input[name='brands']").on("change", function(){
+                var brands = "";
+                $("input[name='brands']:checked").each(function(){
+                    if(brands == "")
+                    {
+                        brands += $(this).val();
+                    }
+                    else{
+                        brands += "," + $(this).val();
+                    }
+                });
+                $("#hdnBrands").val(brands);
+                $("#frmfilter").submit();
+            });
+
+            
+            // FOR CATEGORIES FILTER
+            $("input[name='categories']").on("change", function(){
+                var categories = "";
+                $("input[name='categories']:checked").each(function(){
+                    if(categories == "")
+                    {
+                        categories += $(this).val();
+                    }
+                    else{
+                        categories += "," + $(this).val();
+                    }
+                });
+                $("#hdnCategories").val(categories);
+                $("#frmfilter").submit();
+            });
+
+            //FOR MIN AND MAX PRICE FILTER
+            $("[name='price_range']").on("change", function(){
+                var min = $(this).val().split(',')[0];
+                var max = $(this).val().split(',')[1];
+                // console.log(min);
+                // console.log(max);
+                
+                $("#hdnMinPrice").val(min);
+                $("#hdnMaxPrice").val(max);
+                setTimeout(() => {
+                    $("#frmfilter").submit();
+                }, 2000);
+            });
+        });
+    </script>
+@endpush
